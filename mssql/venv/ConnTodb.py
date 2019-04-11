7ea512947d5ffb3f81bb430c160f5abb904bae86
nvetrov <nvetrov@gmail.com>
2019-04-11 15:48:36 +0300

    Убрать данные

--- mssql/venv/ConnTodb.py
+++ mssql/venv/ConnTodb.py
@@ -2,10 +2,7 @@
 # git remote add origin https://github.com/nvetrov/MSSQL.git
 # git push -u origin master
 import pyodbc
-server = 'tcp:prs043'
-database = 'OST'
-username = 'OSTUser'
-password = 'OSTUser'
+
 
 connection = pyodbc.connect('driver={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 # connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
